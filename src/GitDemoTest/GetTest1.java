f587b49f8c99b479d0f33baad24632de49dfb4c5
Ranjita.Kukanur <Ranjita.Kukanur@HIJ-WS021.INFOVISIONLABS.COM>
2021-12-06 15:28:09 +0530

    commit....

--- src/GitDemoTest/GetTest1.java
+++ src/GitDemoTest/GetTest1.java
@@ -6,14 +6,14 @@ import java.util.Arrays;
 public class GetTest1 {
 
 	public static void main(String[] args) {
-		String arr[]= {"a","b","c"};
+//		String arr[]= {"a","b","c"};
 
 		for(String val:arr) {
 			
 		System.out.println(val);
 		}
 		ArrayList arrl=new ArrayList(Arrays.asList(arr));
-		System.out.println(arrl);
+//		System.out.println(arrl);
 	}
 
 	
