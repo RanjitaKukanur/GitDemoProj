2bf8319a26d179e5779a40d1328574cdc896db70
Ranjita.Kukanur <Ranjita.Kukanur@HIJ-WS021.INFOVISIONLABS.COM>
2021-12-06 15:15:16 +0530

    First Commit...

---
+++ src/GitDemoTest/GetTest1.java
@@ -0,0 +1,21 @@
+package GitDemoTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GetTest1 {
+
+	public static void main(String[] args) {
+		String arr[]= {"a","b","c"};
+
+		for(String val:arr) {
+			
+		System.out.println(val);
+		}
+		ArrayList arrl=new ArrayList(Arrays.asList(arr));
+		System.out.println(arrl);
+	}
+
+	
+
+}
